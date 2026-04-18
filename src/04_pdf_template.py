c3e89d5517f568ba0a3cd1122c1cf4d62acc8dc8
VictorSantanar <victorsantanar605@gmail.com>
2026-04-18 20:56:16 +0200

    Actualizacion

--- src/04_pdf_template.py
+++ src/04_pdf_template.py
@@ -2,14 +2,14 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
-topics = pd.read_csv("topics.csv")
+topics = pd.read_csv("./src/topics.csv")
 
 for index, row in topics.iterrows():
 
     pdf.add_page()
     pdf.set_text_color(100, 100, 100)
-    pdf.set_font(family='Times', style='B', size=12)
-    pdf.cell(w=0, h=12, txt=row['topic'], align='l', ln=1, border=1)
+    pdf.set_font(family='Times', style='B', size=15)
+    pdf.cell(w=10, h=12, txt=row['Topic'], align='l', ln=1, border=0)
     pdf.line(10, 21, 200, 21)
 
 
